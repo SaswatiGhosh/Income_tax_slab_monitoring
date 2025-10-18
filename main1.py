39f0f4faf329718dfa4f96d5df80e3c024a9a25b
4RN4B <arnabjuniormondal123@gmail.com>
2025-10-19 00:34:19 +0530

    Created PDF from html page

--- main1.py
+++ main1.py
@@ -1,23 +1,28 @@
 import pdfkit
 import pdfplumber
+import requests
 
-path_wkthmltopdf = r'C:\Program Files\wkhtmltox\bin\wkhtmltopdf.exe'
+path_wkthmltopdf = r"C:\Program Files\wkhtmltox\bin\wkhtmltopdf.exe"
 config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 
+res = requests.get(
+    "https://www.incometax.gov.in/iec/foportal/help/individual/return-applicable-1"
+).text
+print(res)
 
-url="https://www.incometax.gov.in/iec/foportal/help/individual/return-applicable-1"
-output_pdf="income_tax_page.pdf"
-pdfkit.from_url(url, output_pdf ,  configuration=config)
+url = "https://www.incometax.gov.in/iec/foportal/help/individual/return-applicable-1"
+output_pdf = "income_tax_page.pdf"
+# pdfkit.from_url(url, output_pdf, configuration=config, options=options)
+pdfkit.from_string(res, "output.pdf", configuration=config)
 
 
 with pdfplumber.open(output_pdf) as pdf:
-    first_page=pdf.pages[0]
-    text= first_page.extract_text()
+    first_page = pdf.pages[0]
+    text = first_page.extract_text()
     print(text)
 
-    tables=first_page.extract_tables()
-    l=[]
+    tables = first_page.extract_tables()
+    l = []
     for table in tables:
         for row in table:
             print(row)
-
